baf6e12e61092f4e8b3a9f9e8fdad1e4b699fa64
Tyler Hoyer <tyler@hoyerz.com>
2020-11-30 10:54:56 -0800

    Control to octomap

--- src/pulsefire/control/control.cpp
+++ src/pulsefire/control/control.cpp
@@ -1,31 +1,57 @@
-//#include <gazebo/common/Plugin.hh>
 #include "control.hpp"
+#include <iostream>
+#include <fstream>
 
-//#include "gazebo_plugins/gazebo_ros_camera.h"
-//#include <gazebo/sensors/Sensor.hh>
-//#include <gazebo/sensors/SensorTypes.hh>
-//#include <gazebo_plugins/gazebo_ros_camera_utils.h>
+#define NODE_NAME "controller"
+#define LASER_TOPIC "point_cloud"
 
-#include <sensor_msgs/PointCloud.h>
-#include <geometry_msgs/Point32.h>
+using std::ofstream;
 
 namespace gazebo {
-  // Register this plugin with the simulator
-  GZ_REGISTER_SENSOR_PLUGIN(ControlPlugin)
+  GZ_REGISTER_MODEL_PLUGIN(ControlPlugin)
 
   ControlPlugin::ControlPlugin() :
-    node("control_sensor_plugin"),
-    publisher(node.advertise<sensor_msgs::PointCloud>("depth", 1)) {
-    ROS_INFO("Loaded control_sensor_plugin");
+    map(0.1),
+    node(NODE_NAME),
+    sub(node.subscribe(LASER_TOPIC, 64, &ControlPlugin::OnLaserScan, this)) {
   }
 
   ControlPlugin::~ControlPlugin() {
-    ROS_INFO("Unloaded control_sensor_plugin");
+    ROS_INFO("Unloaded Control Model Plugin");
   }
 
-  void ControlPlugin::OnNewLaserFrame(const float *_image,
-    unsigned int _width, unsigned int _height,
-    unsigned int _depth, const std::string &_format) {
-    ROS_INFO("OnNewLaserFrame: %u, %u, %u\n", _width, _height, _depth);
+  void ControlPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf) {
+    ROS_INFO("Loaded Control Model Plugin");
+    this->model = parent;
+    this->con = event::Events::ConnectWorldUpdateBegin(
+      std::bind(&ControlPlugin::OnUpdate, this));
+    this->sensor = parent->GetLink("sensors");
+  }
+
+  void ControlPlugin::OnUpdate() {
+  }
+
+  void ControlPlugin::OnLaserScan(const sensor_msgs::PointCloud& cloud) {
+    auto& wpose = this->sensor->WorldPose();
+    auto& pos = wpose.Pos();
+    auto& rot = wpose.Rot();
+    octomath::Vector3 trans(pos.X(), pos.Y(), pos.Z());
+    octomap::pose6d pose(trans,
+      octomath::Quaternion(rot.X(), rot.Y(), rot.Z(), rot.W())
+    );
+
+    ROS_INFO("Got Point Cloud");
+    octomap::Pointcloud octomap_cloud;
+    for (const geometry_msgs::Point32& point : cloud.points) {
+      octomap::point3d q(point.x, point.y, point.z);
+        octomap_cloud.push_back(q);
+    }
+    map.insertPointCloud(octomap_cloud, octomap::point3d(0, 0, 0), pose, 9);
+
+    ofstream save("/home/dev/map.bt", ofstream::out | ofstream::binary | ofstream::trunc);
+    bool success = map.writeBinary(save);
+    if (!success)
+      std::cerr << "Error: " << strerror(errno) << "\n";
+    save.close();
   }
 }
--- src/pulsefire/include/control.hpp
+++ src/pulsefire/include/control.hpp
@@ -3,21 +3,31 @@
 #include <string>
 
 #include <ros/ros.h>
-#include <gazebo/GpuRayPlugin.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/sensors/sensors.hh>
+#include <gazebo/common/common.hh>
+#include <sensor_msgs/PointCloud.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <octomap/octomap.h>
 
 namespace gazebo {
-  class ControlPlugin : public GpuRayPlugin {
+  class ControlPlugin : public ModelPlugin {
     public:
       ControlPlugin();
       ~ControlPlugin();
+      void Load(physics::ModelPtr parent, sdf::ElementPtr sdf);
+      void OnUpdate();
+      void OnLaserScan(const sensor_msgs::PointCloud& cloud);
 
     protected:
-      void OnNewLaserFrame(const float *_image,
-        unsigned int _width, unsigned int _height,
-        unsigned int _depth, const std::string &_format) override;
-
+      octomap::OcTree map;
+      physics::ModelPtr model;
+      physics::LinkPtr sensor;
+      event::ConnectionPtr con;
       ros::NodeHandle node;
-      ros::Publisher publisher;
+      ros::Subscriber sub;
   };
-
 }
